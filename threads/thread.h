067f755caade568f96115bd680a2bb188ecf5a0e
julio-delvalle <delvallejulio199@gmail.com>
2024-02-06 04:25:40 +0000

    primeros cambios en base a la guia

--- threads/thread.h
+++ threads/thread.h
@@ -93,6 +93,8 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    uint64_t sleep_time;                /*Tiempo que debe dormir*/
+
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
