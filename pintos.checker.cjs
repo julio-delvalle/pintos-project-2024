4632fc2e3ae048f1585667da6c1b61ed012ee8ac
julio-delvalle <delvallejulio199@gmail.com>
2024-02-10 19:47:13 +0000

    cambios de prueba de autograder

--- pintos.checker.cjs
+++ pintos.checker.cjs
@@ -9,7 +9,7 @@
   * @returns {boolean} retorna true si el test
   * debe pasar*/
 function threads(grade, total) {
-  return grade >= 20
+  return grade >= 0
 }
 
 /** Condición que se debe cumplir para que
