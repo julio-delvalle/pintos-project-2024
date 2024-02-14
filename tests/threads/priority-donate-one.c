7ee15ecdfcf484114ed6cd579e1f6646167b18f7
julio-delvalle <delvallejulio199@gmail.com>
2024-02-14 19:12:17 +0000

    Revert "primeras pruebas donate, cambios a lock_acquire, lock_release"
    This reverts commit cdc692cfa81d19bfc850e109a143ee3c9a1ce8c1.

--- tests/threads/priority-donate-one.c
+++ tests/threads/priority-donate-one.c
@@ -19,7 +19,7 @@ static thread_func acquire1_thread_func;
 static thread_func acquire2_thread_func;
 
 void
-test_priority_donate_one (void)
+test_priority_donate_one (void) 
 {
   struct lock lock;
 
@@ -29,9 +29,7 @@ test_priority_donate_one (void)
   /* Make sure our priority is the default. */
   ASSERT (thread_get_priority () == PRI_DEFAULT);
 
-  printf("ANTES DE LOCK_INIT-----\n\n");
   lock_init (&lock);
-  printf("ANTES DE LOCK_ACQUIRE-----\n\n");
   lock_acquire (&lock);
   thread_create ("acquire1", PRI_DEFAULT + 1, acquire1_thread_func, &lock);
   msg ("This thread should have priority %d.  Actual priority: %d.",
@@ -39,14 +37,13 @@ test_priority_donate_one (void)
   thread_create ("acquire2", PRI_DEFAULT + 2, acquire2_thread_func, &lock);
   msg ("This thread should have priority %d.  Actual priority: %d.",
        PRI_DEFAULT + 2, thread_get_priority ());
-  printf("ANTES DE LOCK_RELEASE-----\n\n");
   lock_release (&lock);
   msg ("acquire2, acquire1 must already have finished, in that order.");
   msg ("This should be the last line before finishing this test.");
 }
 
 static void
-acquire1_thread_func (void *lock_)
+acquire1_thread_func (void *lock_) 
 {
   struct lock *lock = lock_;
 
@@ -57,7 +54,7 @@ acquire1_thread_func (void *lock_)
 }
 
 static void
-acquire2_thread_func (void *lock_)
+acquire2_thread_func (void *lock_) 
 {
   struct lock *lock = lock_;
 
